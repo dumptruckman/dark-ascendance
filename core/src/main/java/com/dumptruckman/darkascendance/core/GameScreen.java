b99b8ad98cf8ebe2f08601d131528903e679d882
Jeremy Wood <farachan@gmail.com>
2013-08-28 12:35:03 -0400

    Changed clear screen code to hopefully work on all systems.

--- core/src/main/java/com/dumptruckman/darkascendance/core/GameScreen.java
+++ core/src/main/java/com/dumptruckman/darkascendance/core/GameScreen.java
@@ -39,8 +39,8 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(final float delta) {
-        Gdx.gl10.glClear(GL10.GL_COLOR_BUFFER_BIT);
-        //Gdx.gl20.glViewport((int) glViewport.x, (int) glViewport.y, (int) glViewport.width, (int) glViewport.height);
+        Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+
         camera.update();
 
         world.setDelta(delta);
