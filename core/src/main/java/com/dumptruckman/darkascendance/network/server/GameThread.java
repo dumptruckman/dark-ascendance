48a46c22b269cd331a9f929a5397ae4715bc6745
Jeremy Wood <farachan@gmail.com>
2013-08-28 23:06:08 -0400

    Updated GameThread to use changed TickRateController.

--- core/src/main/java/com/dumptruckman/darkascendance/network/server/GameThread.java
+++ core/src/main/java/com/dumptruckman/darkascendance/network/server/GameThread.java
@@ -1,19 +1,26 @@
 package com.dumptruckman.darkascendance.network.server;
 
-import com.badlogic.gdx.utils.TimeUtils;
+import com.dumptruckman.darkascendance.util.TickRateController;
 
 public class GameThread extends Thread {
 
-    public static final long TICK_LENGTH = 15000000L;
+    public static final long TICK_LENGTH_MILLIS = 15L;
 
-
+    private TickRateController tickRateController = new TickRateController(TICK_LENGTH_MILLIS);
 
     @Override
     public void run() {
         while (true) {
-            if (gameIsRunningToFast())
+            waitForTickIfNecessary();
         }
     }
 
+    private void waitForTickIfNecessary() {
+        try {
+            tickRateController.waitForTickIfNecessary();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
