83ddb41606d5cd591a2db74be7e89e84fb0a4c7e
Jeremy Wood <farachan@gmail.com>
2013-08-30 10:13:41 -0400

    Fixed EntityFactory imports.

--- core/src/main/java/com/dumptruckman/darkascendance/core/EntityFactory.java
+++ core/src/main/java/com/dumptruckman/darkascendance/core/EntityFactory.java
@@ -2,12 +2,10 @@ package com.dumptruckman.darkascendance.core;
 
 import com.artemis.Entity;
 import com.artemis.World;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.dumptruckman.darkascendance.core.components.*;
-import com.dumptruckman.darkascendance.core.components.Graphics;
-import com.dumptruckman.darkascendance.core.graphics.TextureFactory;
-import com.dumptruckman.darkascendance.core.graphics.TexturePack;
-import com.dumptruckman.darkascendance.core.graphics.Textures;
+import com.dumptruckman.darkascendance.core.components.Controls;
+import com.dumptruckman.darkascendance.core.components.Position;
+import com.dumptruckman.darkascendance.core.components.Thrusters;
+import com.dumptruckman.darkascendance.core.components.Velocity;
 
 public class EntityFactory {
 
