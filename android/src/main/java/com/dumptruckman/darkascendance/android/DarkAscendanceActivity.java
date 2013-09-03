96eed90835599f57baaa4f2dbab25dd12376b221
Jeremy Wood <farachan@gmail.com>
2013-09-03 17:19:24 -0400

    Testing android stuff.

--- android/src/main/java/com/dumptruckman/darkascendance/android/DarkAscendanceActivity.java
+++ android/src/main/java/com/dumptruckman/darkascendance/android/DarkAscendanceActivity.java
@@ -1,19 +1,25 @@
 package com.dumptruckman.darkascendance.android;
 
-import com.dumptruckman.darkascendance.core.DarkAscendance;
-
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.dumptruckman.darkascendance.client.GameSettings;
+import com.dumptruckman.darkascendance.shared.DarkAscendance;
+import recs.Entity;
+import recs.EntityWorld;
 
 public class DarkAscendanceActivity extends AndroidApplication {
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
-			AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-			config.useGL20 = true;
-			initialize(new DarkAscendance(), config);
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        config.useGL20 = true;
+        EntityWorld world = new EntityWorld();
+        Entity entity = new Entity();
+        world.addEntity(entity);
+        //get
+        //initialize(new DarkAscendance(new GameSettings()), config);
+    }
 }
