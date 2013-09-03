1fd0e8036d8381f8fe6b8331445b37ac7857d882
Jeremy Wood <farachan@gmail.com>
2013-09-03 10:19:33 -0400

    Overriden toString() to Entity.

--- core/src/main/java/com/dumptruckman/darkascendance/shared/Entity.java
+++ core/src/main/java/com/dumptruckman/darkascendance/shared/Entity.java
@@ -3,6 +3,8 @@ package com.dumptruckman.darkascendance.shared;
 
 import com.dumptruckman.darkascendance.shared.components.Component;
 
+import java.util.Arrays;
+
 public class Entity extends recs.Entity {
 
     public void addComponent(final Component... components) {
@@ -10,5 +12,20 @@ public class Entity extends recs.Entity {
         for (Component component : components) {
             component.setEntityId(getId());
         }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder components = new StringBuilder();
+        for (recs.Component component : getComponents()) {
+            if (components.length() != 0) {
+                components.append(", ");
+            }
+            components.append(component.getClass());
+        }
+        return "Entity{" +
+                "entityId=" + getId() +
+                ", components=[" + components + "]" +
+                "}";
     }
 }
