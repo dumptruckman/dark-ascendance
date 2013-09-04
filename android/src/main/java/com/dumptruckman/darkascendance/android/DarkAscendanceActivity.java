7eba7a785731d9cf10b8d9803f3ba9b2326024b8
Jeremy Wood <farachan@gmail.com>
2013-09-04 11:05:31 -0400

    Runs on Android.  Lacks controls.

--- android/src/main/java/com/dumptruckman/darkascendance/android/DarkAscendanceActivity.java
+++ android/src/main/java/com/dumptruckman/darkascendance/android/DarkAscendanceActivity.java
@@ -1,13 +1,12 @@
 package com.dumptruckman.darkascendance.android;
 
+import android.graphics.Point;
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.dumptruckman.darkascendance.client.GameSettings;
 import com.dumptruckman.darkascendance.shared.DarkAscendance;
-import recs.Entity;
-import recs.EntityWorld;
 
 public class DarkAscendanceActivity extends AndroidApplication {
 
@@ -16,10 +15,10 @@ public class DarkAscendanceActivity extends AndroidApplication {
         super.onCreate(savedInstanceState);
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
         config.useGL20 = true;
-        EntityWorld world = new EntityWorld();
-        Entity entity = new Entity();
-        world.addEntity(entity);
-        //get
-        //initialize(new DarkAscendance(new GameSettings()), config);
+
+        Point p = new Point();
+        getWindowManager().getDefaultDisplay().getSize(p);
+
+        initialize(new DarkAscendance(new GameSettings(p.x, p.y), "gnarbros.dyndns.org", 25560, 25565), config);
     }
 }
